447b4f06170b02fdbb4dc10a40a07b5a2fdde8d3
William Walsh <wmdwalsh0@gmail.com>
2021-06-22 00:02:02 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove(); 
+const newHeader = document.createElement('h1');
+// newHeader.className = "victory";
+const header = document.getElementById('victory');
+newHeader.innerHTML = "<h1 id='victory'>WILL is the champion</h1>"; 
+// newHeader = document.getElementById('victory');
